d33868acc7a5fbc0f906baaf4b49cae8d9dfeb34
Nick Straw <nick@broxholme.com>
2022-12-03 13:40:44 +0000

    Day3

--- src/Day3.hs
+++ src/Day3.hs
@@ -11,19 +11,15 @@ score c = 1 + ord c - if isUpper c then ord 'A' - 26 else ord 'a'
 
 
 priority1 :: String -> Int
-priority1 s = score c
+priority1 s = score $ head $ filter (`elem` c1) c2
   where
     n = length s
     (c1, c2) = splitAt (n `div` 2) s
-    c = head $ filter (`elem` c1) c2
     
 
 priority2 :: [String] -> Int
-priority2 [s1,s2,s3] = score $ head f2
-  where
-    f1 = filter (`elem` s2) s1
-    f2 = filter (`elem` s3) f1
-priority2 _ = error "!!"
+priority2 (s1:s2:s3:_) = score $ head $ filter (`elem` s3) $ filter (`elem` s2) s1
+priority2 _ = error "error in Day3:priority2 - there aren't 3 strings"
 
 
 day3 :: IO ()
